982d2ee3906cb5e79e6c191e3f8162c8be3d4577
Eaulmesse <theo.guerin01120@gmail.com>
2025-08-11 17:52:57 +0200

    add: secure routes

--- src/audits/audits.controller.ts
+++ src/audits/audits.controller.ts
@@ -1,32 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { AuditsService } from './audits.service';
 import { CreateAuditDto } from './dto/create-audit.dto';
 import { UpdateAuditDto } from './dto/update-audit.dto';
+import { AuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('audits')
 export class AuditsController {
   constructor(private readonly auditsService: AuditsService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createAuditDto: CreateAuditDto) {
     return this.auditsService.create(createAuditDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.auditsService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.auditsService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAuditDto: UpdateAuditDto) {
     return this.auditsService.update(+id, updateAuditDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.auditsService.remove(+id);
--- src/site/site.controller.ts
+++ src/site/site.controller.ts
@@ -1,22 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { SiteService } from './site.service';
 import { CreateSiteDto } from './dto/create-site.dto';
 import { UpdateSiteDto } from './dto/update-site.dto';
+import { AuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('site')
 export class SiteController {
   constructor(private readonly siteService: SiteService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createSiteDto: CreateSiteDto) {
     return this.siteService.create(createSiteDto);
   }
 
+
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.siteService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.siteService.findOne(+id);
@@ -27,6 +32,7 @@ export class SiteController {
     return this.siteService.update(+id, updateSiteDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.siteService.remove(+id);
